1a40ce313f67a99f0598029fe3fb43b19f5679ce
Stu Poole <stuart.f.poole@gmail.com>
2019-11-01 16:21:53 +0000

    Made to slide 18 of the testing powerpoint.

---
+++ Testing/testCatch.cpp
@@ -0,0 +1,6 @@
+//
+// Created by stu on 01/11/2019.
+//
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
---
+++ Testing/testHello.cpp
@@ -0,0 +1,8 @@
+//
+// Created by stu on 01/11/2019.
+//
+#include <iostream>
+
+int main(){
+std::cout << "hello world" << std::endl;
+}
